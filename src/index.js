6380f77608d8ce8093cae461a572e88541c8c895
somemohit <98733767+somemohit@users.noreply.github.com>
2024-01-12 14:01:12 +0530

    jokes page style modification

--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
   HashRouter,
-  Routes,
-  Route,
 } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-
       <App />
-
     </HashRouter>
   </React.StrictMode>
 );
